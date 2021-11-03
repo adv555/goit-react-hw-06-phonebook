5a03770d206da5561f7b41516bb5aabbf7de246f
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-11-03 19:01:44 +0200

    add formatPhoneNumber

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,13 +27,23 @@ export default function Form() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch(addContact(name, number));
+
+    dispatch(addContact(name, formatPhoneNumber(number)));
     reset();
   };
 
   const reset = () => {
     setName('');
     setNumber('');
+  };
+
+  const formatPhoneNumber = number => {
+    //Filter only numbers from the input
+    let cleaned = ('' + number).replace(/\D/g, '');
+    //Check if the input is of correct length
+    let match = cleaned.match(/^(\d{3})(\d{2})(\d{2})$/);
+
+    return match ? +match[1] + '-' + match[2] + '-' + match[3] : number;
   };
 
   const nameInputId = shortid.generate();
